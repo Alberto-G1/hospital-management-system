dfb71aa79bf3133893fe3ed5386d64de6708b69b
Albert-grande <nuwarindaalbert56@gmail.com>
2025-06-18 09:51:33 +0300

    Doctor availability enum, Doctor's setters added and used in the controller.

--- src/main/java/org/example/models/Doctor.java
+++ src/main/java/org/example/models/Doctor.java
@@ -1,5 +1,7 @@
 package org.example.models;
 
+import org.example.models.enums.Availability;
+
 import javax.persistence.*;
 
 @Entity
@@ -13,7 +15,7 @@ public class Doctor {
     private String speciality;
     private String phoneNumber;
     private String email;
-    private boolean isAvailable;
+    private Availability isAvailable;
 
     public Doctor(int doctorID, String firstName, String lastName, String speciality, String phoneNumber, String email) {
         this.doctorID = doctorID;
@@ -22,15 +24,36 @@ public class Doctor {
         this.speciality = speciality;
         this.phoneNumber = phoneNumber;
         this.email = email;
+        this.isAvailable = Availability.UNAVAILABLE;
     }
 
-    public Doctor(String firstName, String lastName, String specialty, String phone, String email) {
+    public Doctor() {
+
     }
+
+    // GETTTERS AND SETTERS
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public void setSpeciality(String speciality) { this.speciality = speciality; }
+
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public void setIsAvailable(Availability isAvailable) { this.isAvailable = isAvailable; }
 
     public int getDoctorID() { return doctorID; }
+
     public String getFirstName() { return firstName; }
+
     public String getLastName() { return lastName; }
+
     public String getSpeciality() { return speciality; }
+
     public String getPhoneNumber() { return phoneNumber; }
+
     public String getEmail() { return email; }
 }
